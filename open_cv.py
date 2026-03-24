deb6393ef73c8d2824c7c2373b54859aea87cfa7
MatheshChandKV <matheshchand.25bai10318@vitbhopal.ac.in>
2026-03-24 14:24:09 +0530

    further into OpencV

--- open_cv.py
+++ open_cv.py
@@ -1,9 +1,16 @@
 #OpenCV
+import numpy as np
 import cv2
-img=cv2.imread('test_image2.png')
+img=cv2.imread('test_image.png')
 img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-img3=cv2.GaussianBlur(img2,(67,67),0)
-cv2.imshow('image',img)
-cv2.imshow('image3',img3)
-cv2.imshow('image2',img2)
-cv2.waitKey(5000)
+img3=cv2.GaussianBlur(img2,(7,7),0)
+img4=cv2.Canny(img,275,300)
+img5=cv2.dilate(img4,(7,7),iterations=1)
+img6=cv2.erode(img5,(7,7),iterations=2)
+#cv2.imshow('image',img)
+#cv2.imshow('image3',img3)
+#cv2.imshow('image2',img2)
+cv2.imshow('image4',img4)
+cv2.imshow('image5',img5)
+cv2.imshow('image6',img6)
+cv2.waitKey(20000)
