40dfb0561598698b1a4d2da526008b8769c6724b
MatheshChandKV <matheshchand.25bai10318@vitbhopal.ac.in>
2026-03-31 00:45:06 +0530

    finalization of image pre-processing

--- open_cv.py
+++ open_cv.py
@@ -1,22 +1,25 @@
-#OpenCV
 import cv2
-img=cv2.imread('test_image6.png')
-b_img=cv2.imread('black screen.png')
-img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
-gray = clahe.apply(img2)
-img3=cv2.GaussianBlur(img2,(3,3),1)
-thresh = cv2.adaptiveThreshold(img3, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11, 2)
-kernel = (2,2)
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-img4=cv2.dilate(img3,kernel,iterations=2)
-cv2.imshow('pre-processed image',img4)
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-for cnt in contours:
-    x,y,w,h = cv2.boundingRect(cnt)
-    char = thresh[y:y+h, x:x+w]
-char = cv2.resize(char, (28,28))
-char = char / 255.0
-print(type(char))
-cv2.waitKey(0)
+import numpy as np
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+    gray = clahe.apply(gray)
+    blur = cv2.GaussianBlur(gray, (3,3), 1)
+    thresh = cv2.adaptiveThreshold(blur, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11, 2)
+    kernel = np.ones((2,2), np.uint8)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
+    characters = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w > 5 and h > 5:
+            char = thresh[y:y+h, x:x+w]
+            char = cv2.copyMakeBorder(char, 5,5,5,5,cv2.BORDER_CONSTANT, value=0)
+            char = cv2.resize(char, (28,28))
+            char = char / 255.0
+            char = char.reshape(1, -1)
+            characters.append(char)
+    return characters
