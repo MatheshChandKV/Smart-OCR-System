e28e0df765c99750d69900a97da4e62927159c4a
MatheshChandKV <matheshchand.25bai10318@vitbhopal.ac.in>
2026-03-30 17:11:37 +0530

    Development and training of ML model

--- model_training.py
+++ model_training.py
@@ -1,23 +1,26 @@
 import numpy as np
-
-def load_images(file):
-    with open(file, 'rb') as f:
-        f.read(16)
-        data = np.frombuffer(f.read(), dtype=np.uint8)
-        return data.reshape(-1, 28, 28)
-
-def load_labels(file):
-    with open(file, 'rb') as f:
-        f.read(8)
-        return np.frombuffer(f.read(), dtype=np.uint8)
+import matplotlib.pyplot as p
 from torchvision import datasets
-
-emnist = datasets.EMNIST(
-    root='./data',
-    split='balanced',
-    train=True,
-    download=True
-)
-X = emnist.data.numpy()
-y = emnist.targets.numpy()
-print(X.shape, y.shape)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+emnist=datasets.EMNIST(root='./data',split='balanced',train=True,download=True)
+x=emnist.data.numpy()
+y=emnist.targets.numpy()
+print("Original shape:",x.shape,y.shape)
+x=np.transpose(x,(0,2,1))
+x=np.flip(x,axis=2)
+print("After orientation fix:",x.shape,y.shape)
+x=x / 255.0
+x=x.reshape(x.shape[0],-1)
+x,_,y,_=train_test_split(x,y,train_size=40000,stratify=y,random_state=42)
+X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.2,stratify=y,random_state=42)
+model=SVC(kernel='rbf')
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print("\nClassification Report:\n")
+print(classification_report(y_test,y_pred))
+p.imshow(X_test[0].reshape(28,28),cmap='gray')
+p.title(f"Actual: {y_test[0]} | Predicted: {y_pred[0]}")
+p.axis('off')
+p.show()
--- open_cv.py
+++ open_cv.py
@@ -6,19 +6,12 @@ img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
 gray = clahe.apply(img2)
 img3=cv2.GaussianBlur(img2,(3,3),1)
-thresh = cv2.adaptiveThreshold(
-    img3, 255,
-    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY,
-    11, 2
-)
+thresh = cv2.adaptiveThreshold(img3, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11, 2)
 kernel = (2,2)
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 img4=cv2.dilate(img3,kernel,iterations=2)
-#cv2.imshow('Original',img)
 cv2.imshow('pre-processed image',img4)
 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 for cnt in contours:
     x,y,w,h = cv2.boundingRect(cnt)
     char = thresh[y:y+h, x:x+w]
@@ -26,7 +19,4 @@ char = cv2.resize(char, (28,28))
 char = char / 255.0
 print(type(char))
 cv2.waitKey(0)
-#cv2.imshow('Dilated',img4)
-#cv2.imshow('Eroded',img5)
-#cv2.imshow('Median Blurred of edges',img7)
 
